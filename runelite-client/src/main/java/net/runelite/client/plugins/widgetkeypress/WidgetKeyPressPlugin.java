1fa51be9284ed9ca67996b7a3ab513962f4316c3
Harry Singh <>
2021-10-23 23:07:57 +1100

    Close bank

--- runelite-client/src/main/java/net/runelite/client/plugins/widgetkeypress/WidgetKeyPressPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/widgetkeypress/WidgetKeyPressPlugin.java
@@ -2,10 +2,13 @@ package net.runelite.client.plugins.widgetkeypress;
 
 import com.google.common.base.Splitter;
 import com.google.inject.Provides;
+import com.sun.jna.platform.win32.WinDef;
 import net.runelite.api.Client;
 import net.runelite.api.KeyCode;
 import net.runelite.api.events.GameTick;
+import net.runelite.api.events.MenuOpened;
 import net.runelite.api.widgets.WidgetID;
+import net.runelite.api.widgets.WidgetInfo;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
@@ -18,6 +21,7 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 @PluginDescriptor(
 		name = "Widget KeyPress",
@@ -33,6 +37,8 @@ public class WidgetKeyPressPlugin extends Plugin {
 			.on("\n")
 			.omitEmptyStrings()
 			.trimResults();
+	private Boolean closeBank = false;
+	private Integer closeBankTick = 0;
 
 	@Inject
 	private WidgetKeyPressConfig config;
@@ -44,7 +50,7 @@ public class WidgetKeyPressPlugin extends Plugin {
 	private ExtUtils extUtils;
 
 	@Provides
-	WidgetKeyPressConfig provideConfig(ConfigManager configManager) {
+	WidgetKeyPressConfig getConfig(ConfigManager configManager) {
 		return configManager.getConfig(WidgetKeyPressConfig.class);
 	}
 
@@ -56,70 +62,98 @@ public class WidgetKeyPressPlugin extends Plugin {
 	@Subscribe
 	public void onGameTick(GameTick event) {
 		var widget = client.getWidget(WidgetID.MULTISKILL_MENU_GROUP_ID, 14);
+		var bank = client.getWidget(WidgetInfo.BANK_CONTAINER);
 
-		if (widget == null) {
-			return;
-		}
+		if (widget != null && !items.isEmpty()) {
+			var widgetName = widget.getName().toLowerCase();
 
-		var widgetName = widget.getName().toLowerCase();
-
-		items.forEach(item -> {
-			if (!widgetName.contains(item.name)) {
-				return;
-			}
-
-			var canvas = client.getCanvas();
-
-			if (canvas == null) {
-				return;
-			}
-
-			var keyPressed = new KeyEvent(canvas, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, 54, KeyEvent.CHAR_UNDEFINED);
-			KeyEvent keyTyped = null;
-			var keyReleased = new KeyEvent(canvas, KeyEvent.KEY_RELEASED, System.currentTimeMillis(), 0, 54, KeyEvent.CHAR_UNDEFINED);
-
-			switch (item.key) {
-				case SPACE:
-					try {
-						Robot r = new Robot();
-						r.keyPress(KeyEvent.VK_SPACE);
-					} catch (AWTException e) {
-						e.printStackTrace();
-					}
+			items.forEach(item -> {
+				if (!widgetName.contains(item.name)) {
 					return;
-				case ONE:
-					keyTyped = new KeyEvent(canvas, KeyEvent.KEY_TYPED, System.currentTimeMillis(), 0, KeyEvent.VK_UNDEFINED, (char)KeyEvent.VK_1);
-					break;
-				case TWO:
-					keyTyped = new KeyEvent(canvas, KeyEvent.KEY_TYPED, System.currentTimeMillis(), 0, KeyEvent.VK_UNDEFINED, (char)KeyEvent.VK_2);
-					break;
-				case THREE:
-					keyTyped = new KeyEvent(canvas, KeyEvent.KEY_TYPED, System.currentTimeMillis(), 0, KeyEvent.VK_UNDEFINED, (char)KeyEvent.VK_3);
-					break;
-				case FOUR:
-					keyTyped = new KeyEvent(canvas, KeyEvent.KEY_TYPED, System.currentTimeMillis(), 0, KeyEvent.VK_UNDEFINED, (char)KeyEvent.VK_4);
-					break;
-				case FIVE:
-					keyTyped = new KeyEvent(canvas, KeyEvent.KEY_TYPED, System.currentTimeMillis(), 0, KeyEvent.VK_UNDEFINED, (char)KeyEvent.VK_5);
-					break;
-				case SIX:
-					keyTyped = new KeyEvent(canvas, KeyEvent.KEY_TYPED, System.currentTimeMillis(), 0, KeyEvent.VK_UNDEFINED, (char)KeyEvent.VK_6);
-					break;
-				case SEVEN:
-					keyTyped = new KeyEvent(canvas, KeyEvent.KEY_TYPED, System.currentTimeMillis(), 0, KeyEvent.VK_UNDEFINED, (char)KeyEvent.VK_7);
-					break;
-				case EIGHT:
-					keyTyped = new KeyEvent(canvas, KeyEvent.KEY_TYPED, System.currentTimeMillis(), 0, KeyEvent.VK_UNDEFINED, (char)KeyEvent.VK_8);
-					break;
-				case NINE:
-					keyTyped = new KeyEvent(canvas, KeyEvent.KEY_TYPED, System.currentTimeMillis(), 0, KeyEvent.VK_UNDEFINED, (char)KeyEvent.VK_9);
-					break;
+				}
+
+				var canvas = client.getCanvas();
+
+				if (canvas == null) {
+					return;
+				}
+
+				var keyPressed = new KeyEvent(canvas, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, 54, KeyEvent.CHAR_UNDEFINED);
+				KeyEvent keyTyped = null;
+				var keyReleased = new KeyEvent(canvas, KeyEvent.KEY_RELEASED, System.currentTimeMillis(), 0, 54, KeyEvent.CHAR_UNDEFINED);
+
+				switch (item.key) {
+					case SPACE:
+						try {
+							Robot r = new Robot();
+							r.keyPress(KeyEvent.VK_SPACE);
+						} catch (AWTException e) {
+							e.printStackTrace();
+						}
+						return;
+					case ONE:
+						keyTyped = new KeyEvent(canvas, KeyEvent.KEY_TYPED, System.currentTimeMillis(), 0, KeyEvent.VK_UNDEFINED, (char)KeyEvent.VK_1);
+						break;
+					case TWO:
+						keyTyped = new KeyEvent(canvas, KeyEvent.KEY_TYPED, System.currentTimeMillis(), 0, KeyEvent.VK_UNDEFINED, (char)KeyEvent.VK_2);
+						break;
+					case THREE:
+						keyTyped = new KeyEvent(canvas, KeyEvent.KEY_TYPED, System.currentTimeMillis(), 0, KeyEvent.VK_UNDEFINED, (char)KeyEvent.VK_3);
+						break;
+					case FOUR:
+						keyTyped = new KeyEvent(canvas, KeyEvent.KEY_TYPED, System.currentTimeMillis(), 0, KeyEvent.VK_UNDEFINED, (char)KeyEvent.VK_4);
+						break;
+					case FIVE:
+						keyTyped = new KeyEvent(canvas, KeyEvent.KEY_TYPED, System.currentTimeMillis(), 0, KeyEvent.VK_UNDEFINED, (char)KeyEvent.VK_5);
+						break;
+					case SIX:
+						keyTyped = new KeyEvent(canvas, KeyEvent.KEY_TYPED, System.currentTimeMillis(), 0, KeyEvent.VK_UNDEFINED, (char)KeyEvent.VK_6);
+						break;
+					case SEVEN:
+						keyTyped = new KeyEvent(canvas, KeyEvent.KEY_TYPED, System.currentTimeMillis(), 0, KeyEvent.VK_UNDEFINED, (char)KeyEvent.VK_7);
+						break;
+					case EIGHT:
+						keyTyped = new KeyEvent(canvas, KeyEvent.KEY_TYPED, System.currentTimeMillis(), 0, KeyEvent.VK_UNDEFINED, (char)KeyEvent.VK_8);
+						break;
+					case NINE:
+						keyTyped = new KeyEvent(canvas, KeyEvent.KEY_TYPED, System.currentTimeMillis(), 0, KeyEvent.VK_UNDEFINED, (char)KeyEvent.VK_9);
+						break;
+				}
+
+				canvas.dispatchEvent(keyPressed);
+				canvas.dispatchEvent(keyTyped);
+				canvas.dispatchEvent(keyReleased);
+			});
+		} else if (bank != null && !closeBank) {
+			var inventory = client.getWidget(WidgetInfo.INVENTORY);
+			var items = inventory.getWidgetItems();
+
+			if (items.size() != 28) {
+				return;
 			}
 
-			canvas.dispatchEvent(keyPressed);
-			canvas.dispatchEvent(keyTyped);
-			canvas.dispatchEvent(keyReleased);
-		});
+			var bows = (int) items.stream().filter(item -> item.getId() == 1777).count();
+			var bowStrings = (int) items.stream().filter(item -> item.getId() == 66).count();
+
+			if (bows == 14 && bowStrings == 14) {
+				try {
+					Robot r = new Robot();
+					r.keyPress(KeyEvent.VK_ESCAPE);
+				} catch (AWTException e) {
+					e.printStackTrace();
+				}
+			}
+		} else if (closeBank) {
+			if (closeBankTick != 1) {
+				closeBankTick++;
+			} else {
+
+				// Press space
+
+				closeBank = false;
+				closeBankTick = 0;
+			}
+		}
 	}
 
 	public void updateMap(String swaps)
